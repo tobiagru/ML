5297675bbf9fa5937f8eba615e62c60725a8d4bd
soroa <soroa@student.ethz.ch>
2016-10-30 17:22:33 +0100

    added function to parse xml files

--- src/predict.py
+++ src/predict.py
@@ -9,6 +9,8 @@ from sklearn.svm import LinearSVC
 from sklearn.decomposition import PCA
 import random
 from sklearn import datasets, linear_model
+from xml.dom import minidom
+
 
 training_samples = 278
 test_samples = 138
@@ -92,3 +94,44 @@ model = LinearSVC()
 model.fit(train,train_labels.values.ravel())
 print(model.predict(test))
 print("Done")
+
+
+# returns numpy array of data. Dimensions: samplesCount X 6
+# features are [abs CSM volume,  abs GM volume, abs WM volume, rel CSM volume, rel GM volume, rel WM volume]
+#samples count:
+# 	training: 278
+# 	testing: 138
+
+def parseDataSetXML(samplesCount,filepath)
+	files = [f for f in os.listdir(filepath)]
+	features_matrix = np.zeros((samplesCount,6))
+
+	cnt =0
+	for f in files:
+		# print(f)
+		if(f==".DS_Store"):
+			continue
+		xmldoc = minidom.parse('./trainsets/'+f)
+		filenmae = xmldoc.getElementsByTagName('file')[0].childNodes[0].data
+		index = filenmae.split("_")[1]
+		# print(index)
+		values = xmldoc.getElementsByTagName('file')[0].childNodes[0].data
+
+		if(xmldoc.getElementsByTagName('vol_abs_CGW')):
+			vol_abs_CGW = xmldoc.getElementsByTagName('vol_abs_CGW')[0].childNodes[0].data
+		else:
+			print("Attributes missing at index " + index)
+		if(xmldoc.getElementsByTagName('vol_rel_CGW')):
+			vol_rel_CGW = xmldoc.getElementsByTagName('vol_rel_CGW')[0].childNodes[0].data
+		else:
+			print("Attributes missing at index " + index)
+
+		cAbs = vol_abs_CGW.split("[")[1].split(" ")[0]
+		gAbs = vol_abs_CGW.split("[")[1].split(" ")[1]
+		wAbs = vol_abs_CGW.split("[")[1].split(" ")[2]
+		cRel = vol_rel_CGW.split("[")[1].split(" ")[0]
+		gRel = vol_rel_CGW.split("[")[1].split(" ")[1]
+		wRel = vol_rel_CGW.split("[")[1].split(" ")[2]
+
+		features_matrix[int(index)-1,:] = np.array([float(cAbs),float(gAbs),float(wAbs),float(cRel),float(gRel),float(wRel)])
+		return features_matrix
